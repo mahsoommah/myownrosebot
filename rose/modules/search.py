63fd65a630d0b6d7b1f8b692632cfe62fa9ddc9b
Mahsoom <97832473+mahsoommah@users.noreply.github.com>
2022-04-11 13:03:53 +0530

    Update search.py

--- rose/modules/search.py
+++ rose/modules/search.py
@@ -152,7 +152,7 @@ async def google(_, message):
     app_details += (
             "\n<code>Features:</code> <a href='" + app_link + "'>View in Play Store</a>"
         )
-    app_details += "\n\n🔰 @hijabqueen_bot 🔰"
+    app_details += "\n\n🔰 @ghost_rosebot 🔰"
     await message.reply_photo(
                         photo = app_link,
                         caption = app_details, 
